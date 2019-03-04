c736ad5f5b2ff7518f52cf41cd64763b53789839
Copetan <14883143+Copetan@users.noreply.github.com>
2019-03-03 21:02:56 -0800

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+import events.NewEvents;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.JDA;
+
+public class main {
+    private static JDA jda;
+
+    public static void main(String[] args) throws Exception {
+        jda = new JDABuilder(AccountType.BOT).setToken("NTUxOTEwOTM3MDc5MTg1NDEy.D136pQ.HDy6sNxM0XVOVbucdDdw2x4pjr4").buildBlocking();
+        jda.addEventListener(new NewEvents());
+
+    }
+}
