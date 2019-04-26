f66356b2d6260a7c20b7db8dc4267a169963d715
Thomas <turpleturtle12@gmail.com>
2019-04-26 11:35:59 -0700

    Added Some Stuff

---
+++ src/main/java/methodsnstuff/Calculator.java
@@ -0,0 +1,35 @@
+package methodsnstuff;
+
+import adts.Stack;
+
+public class Calculator {
+
+    public double calculate(String toAdd){
+
+        postfixToInfix(toAdd);
+        return 0;
+    }
+
+    private String postfixToInfix(String toConvert ){
+        return "";
+    }
+
+
+    private Double calculatePostfix(String postFixExpression){
+
+        Stack Numbers = new Stack();
+
+
+        return 0.0;
+
+
+
+
+
+    }
+
+
+
+
+
+}
